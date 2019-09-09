5ab548d59ef7fac211cf2f513986c9dd2fe33dab
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-09-09 00:45:56 -0500

    Update forward.h

--- forward.h
+++ forward.h
@@ -10,51 +10,143 @@ class ForwardList : public List<T> {
         ForwardList() : List<T>() {}
 
         T front() {
-            // TODO
+            if( this->head != nullptr){
+                return this->head->data;
+            } throw new out_of_range("The list is empty");
         }
 
         T back() {
-            // TODO
+            if( this->tail != nullptr){
+                return this->tail->data;
+            } throw new out_of_range("The list is empty");
+
         }
 
         void push_front(T value) {
-            // TODO
+            auto *temp = new Node<T>(value);
+            if(empty()){
+                this->head = temp;
+                this->tail = temp;
+            }
+            else{
+                temp->next = this->head;
+                this->head = temp;
+            }
+
+            this->nodes += 1;
         }
 
         void push_back(T value) {
-            // TODO
+            auto *temp = new Node<T>(value);
+            if(empty()){
+                this->head = temp;
+                this->tail = temp;
+            }
+            else{
+                this->tail->next = temp;
+                this->tail = temp;
+            }
+            this->nodes += 1;
         }
 
         void pop_front() {
-            // TODO
+            auto *temp = this->head;
+            if(empty()) {
+                throw new out_of_range("The list is empty");
+            }
+            else if(this->nodes == 1){
+                delete this->head;
+            }
+            else{
+                this->head = this->head->next;
+                delete temp;
+                this->nodes -=1;
+            }
         }
 
         void pop_back() {
-            // TODO
+            if(empty()) {
+                throw new out_of_range("The list is empty");
+            }
+            else if(this->nodes == 1){
+                delete this->tail;
+            }
+            else {
+                auto *temp = this->tail;
+                auto *rec = this->head;
+                while (rec->next != this->tail) {
+                    rec = rec->next;
+                }
+                rec->next = nullptr;
+                this->tail = rec;
+                delete temp;
+                this->nodes -=1;
+            }
         }
 
         T operator[](int index) {
-            // TODO
+            if(index >= this->nodes){
+                throw new out_of_range("Out of range");
+            }
+            else{
+                auto*temp = this->head;
+                for(int i = 0; i < index; i++){
+                    temp = temp->next;
+                }
+                return temp->data;
+            }
         }
 
         bool empty() {
-            // TODO
+            return this->head == nullptr && this->tail == nullptr;
         }
 
         int size() {
-            // TODO
+           return this->nodes;
         }
 
         void clear() {
-            // TODO
+            this->head->killSelf();
+            this->nodes = 0;
         }
 
         void sort() {
-            // TODO
+            auto* temp = this->head;
+            int size = this->nodes;
+            T * arr = new T[size];
+            for(int i  = 0; i <size; i++){
+                arr[i] = temp->data;
+                temp = temp->next;
+            }
+            // shell sort
+            for (int gap =size / 2; gap > 0; gap /= 2) {
+                for (int i = gap; i < size; i += 1) {
+                    int t = arr[i];
+                    int j;
+                    for (j = i; j >= gap && arr[j - gap] > t; j -= gap)
+                        arr[j] = arr[j - gap];
+                    arr[j] = t;
+                }
+            }
+            temp = this->head;
+            for(int i  = 0; i <size; i++){
+                temp->data = arr[i];
+                temp->next;
+            }
         }
     
         void reverse() {
-            // TODO
+            auto* temp = this->head;
+            int size=this->nodes;
+            T * arr  = new T[size];
+            for( int i = 0; i < size; i ++){
+                arr[i] = temp->data;
+                temp =temp->next;
+            }
+            temp = this->head;
+            for(int i = size-1 ; i >= 0 ; i ++){
+                temp->data = arr[i];
+            }
         }
 
         string name() {
@@ -70,7 +162,13 @@ class ForwardList : public List<T> {
         }
 
         void merge(ForwardList<T> list) {
-            // TODO
+            int size = list.size();
+            auto * temp = list.head;
+            for(int i = 0; i < size; i++){
+                this->push_back(temp->data);
+                temp =temp->next;
+            }
+
         }
 };
 

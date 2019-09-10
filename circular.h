144f52ebaf7380f454ed5cfcac63946e882ffefb
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-09-09 22:34:05 -0500

    Update circular.h

--- circular.h
+++ circular.h
@@ -17,23 +17,87 @@ class CircularLinkedList : public List<T> {
         }
 
         void push_front(T value) {
-
+            auto* temp = new Node<T>(value);
+            if(empty()){
+                this->head = temp;
+                this->tail = temp;
+                this->head->prev = this->tail;
+                this->tail->next = this->head;
+            }
+            else{
+                temp->next = this->head;
+                temp->prev = this->tail;
+                this->head->prev = temp;
+                this->tail->next = temp;
+                this->head = temp;
+            }
+            this->nodes++;
         }
 
         void push_back(T value) {
-
+            auto* temp = new Node<T>(value);
+            if(empty()){
+                this->head = temp;
+                this->tail = temp;
+                this->head->prev = this->tail;
+                this->tail->next = this->head;
+            }else{
+                temp->prev = this->tail;
+                this->tail->next= temp;
+                temp->next = this->head;
+                this->head->prev= temp;
+                this->tail = temp;
+            }
+            this->nodes++;
         }
 
         void pop_front() {
-
+            if(empty()) {
+                throw new out_of_range("The list is empty");
+            }
+            else if(this->nodes ==1){
+                delete this->head;
+                this->head == nullptr;
+                this->tail == nullptr;
+                this->nodes = 0;
+            }
+            else{
+                auto* temp = this->head;
+                this->tail->next = this->head->next;
+                this->head->next->prev = this->tail;
+                this->head = this->head->next;
+                delete temp;
+                this->nodes--;
+            }
         }
 
         void pop_back() {
-
+            if(empty()) {
+                throw new out_of_range("The list is empty");
+            }
+            else if(this->nodes ==1){
+                delete this->head;
+                this->head == nullptr;
+                this->tail == nullptr;
+                this->nodes = 0;
+            }
+            else{
+                auto* temp = this->tail;
+                this->head->prev = this->tail->prev;
+                this->tail->prev->next = this->head;
+                this->tail = this->tail->prev;
+                delete temp;
+                this->nodes--;
+            }
         }
 
         T operator[](int index) {
-
+            int lim = index%(this->nodes);
+            auto *dat = this->head;
+            for (int i = 0; i < lim ; i++) {
+                dat = dat->next;
+            }
+            return dat->data;
         }
 
         bool empty() {
@@ -45,15 +109,48 @@ class CircularLinkedList : public List<T> {
         }
 
         void clear() {
-
+            this->tail->next = nullptr;
+            this->head->killSelf();
+            this->nodes = 0;
         }
 
         void sort() {
-
+            auto* temp = this->head;
+            int size = this->nodes;
+            T * arr = new T[size];
+            for(int i  = 0; i <size; i++){
+                arr[i] = temp->data;
+                temp = temp->next;
+            }
+            // shell sort
+            for (int gap =size / 2; gap > 0; gap /= 2) {
+                for (int i = gap; i < size; i += 1) {
+                    int t = arr[i];
+                    int j;
+                    for (j = i; j >= gap && arr[j - gap] > t; j -= gap)
+                        arr[j] = arr[j - gap];
+                    arr[j] = t;
+                }
+            }
+            temp = this->head;
+            for(int i  = 0; i <size; i++){
+                temp->data = arr[i];
+                temp->next;
+            }
         }
     
         void reverse() {
-
+            auto* temp = this->head;
+            int size=this->nodes;
+            T * arr  = new T[size];
+            for( int i = 0; i < size; i ++){
+                arr[i] = temp->data;
+                temp =temp->next;
+            }
+            temp = this->head;
+            for(int i = size-1 ; i >= 0 ; i ++){
+                temp->data = arr[i];
+            }
         }
 
         string name() {
@@ -69,7 +166,12 @@ class CircularLinkedList : public List<T> {
         }
 
         void merge(CircularLinkedList<T> list) {
-
+            int size = list.size();
+            auto * temp = list.head;
+            for(int i = 0; i < size; i++){
+                this->push_back(temp->data);
+                temp =temp->next;
+            }
         }
 };
 
